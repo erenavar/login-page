63c77f3f251f7033a1610301ada55c9ae85014a0
Eren AVAR <eren.avar@hotmail.com>
2024-05-10 12:57:34 +0200

    create labels and input areas

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Name</Text>
+      <TextInput style={styles.textInput} placeholder="Your Name" />
+
+      <Text>Surname</Text>
+      <TextInput style={styles.textInput} placeholder="Your Surname" />
     </View>
   );
 }
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    height: 50,
+    width: "80%",
+    textAlign: "center",
+    marginVertical: 10,
   },
 });
