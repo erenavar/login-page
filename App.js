770bbda1ca855e23f7c8e10272271254a0ad89e1
Eren AVAR <eren.avar@hotmail.com>
2024-05-11 22:39:44 +0200

    add stack navigator structures

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import LoginScreen from "./src/screens/LoginScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
