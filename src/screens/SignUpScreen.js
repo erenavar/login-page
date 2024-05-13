2d3a6d3f1fe6a5ec6c3b8796ab8cb97bcc3023fa
Eren AVAR <eren.avar@hotmail.com>
2024-05-13 17:23:35 +0200

    add custom text input to sign up page

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,10 +1,35 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import CustomTextInput from "../components/CustomTextInput";
 
 const SignUpScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
       <Text>SignUpScreen</Text>
+      <CustomTextInput
+        title="Name"
+        isSecureText={false}
+        handleOnChangeText={setName}
+        handleValue={name}
+        handlePlaceHolder="Text Your Name"
+      />
+      <CustomTextInput
+        title="email"
+        isSecureText={false}
+        handleOnChangeText={setEmail}
+        handleValue={email}
+        handlePlaceHolder="Text Your Email"
+      />
+      <CustomTextInput
+        title="Password"
+        isSecureText={true}
+        handleOnChangeText={setPassword}
+        handleValue={password}
+        handlePlaceHolder="Text Your Password"
+      />
     </View>
   );
 };
