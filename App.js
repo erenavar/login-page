5fb8f57c4fac7d81fe0033d8b22cf4b73e6fc610
Eren AVAR <eren.avar@hotmail.com>
2024-05-15 17:30:13 +0200

    add redux  foundational codes

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import RootNavigation from "./src/navigation/RootNavigation";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const App = () => {
-  return <RootNavigation />;
+  return (
+    <Provider store={store}>
+      <RootNavigation />;
+    </Provider>
+  );
 };
 
 export default App;
