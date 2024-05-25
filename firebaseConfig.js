7e5ab077a99d0dd7c18ac2320fa1ccb58d52d930
Eren AVAR <eren.avar@hotmail.com>
2024-05-25 22:38:57 +0200

    create firebaseconfig folfer and copy initialize firebase code

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdEHoEtsEle0CH0fjHY9__54y1FRVMXIs",
+  authDomain: "login-page-b1c90.firebaseapp.com",
+  projectId: "login-page-b1c90",
+  storageBucket: "login-page-b1c90.appspot.com",
+  messagingSenderId: "853125932218",
+  appId: "1:853125932218:web:e933621633553c602e0e83",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
