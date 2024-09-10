251d0842d988b3e21eb3a507605f3b271392b328
Eren AVAR <eren.avar@hotmail.com>
2024-09-10 19:50:52 +0300

    add fetch function for firestore

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
 
 const HomeScreen = () => {
+  const sendData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "cars"), {
+        brand: "Tesla",
+        model: "x",
+        modelYear: 2020,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
